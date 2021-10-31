56b2265f942e6bd5ce332628f3c2a62910ccff51
Bilal ATLI <ytbilalatli@gmail.com>
2021-10-31 21:47:16 +0300

    Add selection sort algorithm

---
+++ src/sorting/algorithms/selection-sort.js
@@ -0,0 +1,17 @@
+const selectionSort = (arr) => {
+    let min;
+    for (let i = 0; i < arr.length; i++) {
+        min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            [arr[i], arr[min]] = [arr[min], arr[i]];
+        }
+    }
+    return arr;
+};
+
+module.exports = selectionSort;
