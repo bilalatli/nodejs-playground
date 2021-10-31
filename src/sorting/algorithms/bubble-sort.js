8a1d39b1d04164c2095c962e1bbe00baa65ddb61
Bilal ATLI <ytbilalatli@gmail.com>
2021-10-31 21:35:30 +0300

    Bubble-sort fixed

--- src/sorting/algorithms/bubble-sort.js
+++ src/sorting/algorithms/bubble-sort.js
@@ -1,8 +1,9 @@
 const bubbleSort = (arr) => {
-    const count = arr.length;
-    for (let j = 0; j < (count - 1); j++) {
-        for (let i = 0; i < (count - j - 1); i++) {
-            [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j + 1] < arr[j]) {
+                [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]]
+            }
         }
     }
     return arr;
